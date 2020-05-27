e467d6e9011edd9c7282579aa5414aa2bb1d558d
Yan Chen <chen.yan3@husky.neu.edu>
2020-05-26 23:40:57 -0400

    Add files via upload

---
+++ ass1-server-java/src/main/webapp/precache-manifest.31bc941d6924b19601a05e81ff3d7a93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17552b5b9a395137fd9fa5293006b54f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb334eda3373f74939e",
+    "url": "/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "5dc2c9208f2ae11c65c0",
+    "url": "/static/css/main.80849204.chunk.css"
+  },
+  {
+    "revision": "9fb334eda3373f74939e",
+    "url": "/static/js/2.a00c4593.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a00c4593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc2c9208f2ae11c65c0",
+    "url": "/static/js/main.9cd36bcc.chunk.js"
+  },
+  {
+    "revision": "1d2824e1fb3dbd84bf4e",
+    "url": "/static/js/runtime-main.d310af3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
